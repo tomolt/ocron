709b60b58fa9e7855c3f57bec741ae675d0b852c
Thomas Oltmann <thomas.oltmann.hhg@gmail.com>
2020-06-16 16:45:54 +0200

    Separated declarations from code.

--- ocrond.c
+++ ocrond.c
@@ -16,9 +16,9 @@
 #define CRONTAB       "./crontab"
 #define CRON_D        "./cron.d"
 #define LOGIDENT      "crond"
-#define CATCHUP_LIMIT 60
 #define STDIN_TEMP    "/tmp/crond.stdin.XXXXXX"
 #define SHELL         "/bin/sh"
+#define CATCHUP_LIMIT 60
 
 #define MINUTES_MASK 0xFFFFFFFFFFFFFFFLL
 #define HOURS_MASK   0xFFFFFFL
@@ -33,6 +33,8 @@
 #define VALID_DAY(job, mday, wday) (VALID_MDAY(job, mday) || VALID_WDAY(job, wday))
 #define VALID_MONTH(job, month) ((job).months >> (month) & 1)
 #define VALID_DATE(job, mday, wday, month) (VALID_DAY(job, mday, wday) && VALID_MONTH(job, month))
+#define IS_SPACE(c) ((c) == ' ' || (c) == '\t' || (c) == '\r')
+#define IS_DIGIT(c) ((c) >= '0' && (c) <= '9')
 
 static const char *no_aliases[] = { NULL };
 static const char *months_aliases[] = {
@@ -109,17 +111,22 @@ find_eol(char *line)
 static void
 next_tm(struct Job job, struct tm *tm_ptr)
 {
-	struct tm tm = *tm_ptr;
+	struct tm tm;
+	long long minutes_left;
+	long hours_left;
+	int today_alright;
+
+	tm = *tm_ptr;
 	tm.tm_sec = 0;
 	tm.tm_isdst = -1;
 
-	int today_alright = VALID_DATE(job, tm.tm_mday, tm.tm_wday, tm.tm_mon);
+	today_alright = VALID_DATE(job, tm.tm_mday, tm.tm_wday, tm.tm_mon);
 
 	/* Determine minute, and exit early if possible. */
 	assert(job.minutes != 0);
 	if (today_alright && VALID_HOUR(job, tm.tm_hour)) {
 		++tm.tm_min;
-		long long minutes_left = job.minutes & ~0ULL << tm.tm_min;
+		minutes_left = job.minutes & ~0ULL << tm.tm_min;
 		if (minutes_left != 0LL) {
 			tm.tm_min = ffsll(minutes_left) - 1;
 			goto finished;
@@ -131,7 +138,7 @@ next_tm(struct Job job, struct tm *tm_ptr)
 	assert(job.hours != 0);
 	if (today_alright) {
 		++tm.tm_hour;
-		long hours_left = job.hours & ~0UL << tm.tm_hour;
+		hours_left = job.hours & ~0UL << tm.tm_hour;
 		if (hours_left != 0L) {
 			tm.tm_hour = ffsl(hours_left) - 1;
 			goto finished;
@@ -164,6 +171,7 @@ heapify_jobs(int idx)
 {
 	struct Job tmp;
 	int left, right, min;
+
 	for (;;) {
 		left = 2 * idx + 1;
 		right = left + 1;
@@ -186,6 +194,7 @@ static void
 update_job(int idx, time_t now)
 {
 	struct tm tm;
+
 	localtime_r(&now, &tm);
 	next_tm(jobs[idx], &tm);
 	jobs[idx].time = mktime(&tm);
@@ -195,6 +204,7 @@ static void
 init_jobs(time_t now)
 {
 	int i;
+
 	for (i = 0; i < numJobs; ++i) {
 		update_job(i, now);
 	}
@@ -209,7 +219,7 @@ static char *
 read_file(const char *filename)
 {
 	struct stat info;
-	char *text;
+	char *contents;
 	ssize_t off = 0, ret;
 	int fd;
 
@@ -218,30 +228,26 @@ read_file(const char *filename)
 
 	if (fstat(fd, &info) < 0) die("Can't stat %s: %m", filename);
 
-	text = malloc(info.st_size + 1);
-	if (text == NULL) die("Can't allocate enough memory.");
-	text[info.st_size] = 0;
+	contents = malloc(info.st_size + 1);
+	if (contents == NULL) die("Can't allocate enough memory.");
+	contents[info.st_size] = 0;
 
 	while (off < info.st_size) {
-		ret = read(fd, text + off, info.st_size - off);
+		ret = read(fd, contents + off, info.st_size - off);
 		if (ret < 0) die("Can't read %s: %m", filename);
 		off += ret;
 	}
 
 	close(fd);
 
-	return text;
+	return contents;
 }
 
 static int
 skip_space(void)
 {
-	if (!(*text == ' ' || *text == '\t' || *text == '\r')) {
-		return -1;
-	}
-	do {
-		++text;
-	} while (*text == ' ' || *text == '\t' || *text == '\r');
+	if (!IS_SPACE(*text)) return -1;
+	do ++text; while (IS_SPACE(*text));
 	return 0;
 }
 
@@ -249,10 +255,11 @@ static int
 parse_number(unsigned int *number)
 {
 	unsigned int num = 0;
-	if (!(*text >= '0' && *text <= '9')) return -1;
+
+	if (!IS_DIGIT(*text)) return -1;
 	do {
 		num = num * 10 + *text++ - '0';
-	} while (*text >= '0' && *text <= '9');
+	} while (IS_DIGIT(*text));
 	*number = num;
 	return 0;
 }
@@ -260,11 +267,13 @@ parse_number(unsigned int *number)
 static int
 parse_value(const char *aliases[], unsigned int *number)
 {
-	if (*text >= '0' && *text <= '9') {
+	unsigned int i;
+
+	if (IS_DIGIT(*text)) {
 		if (parse_number(number) < 0) return -1;
 		return 0;
 	}
-	for (unsigned int i = 0; aliases[i] != NULL; ++i) {
+	for (i = 0; aliases[i] != NULL; ++i) {
 		if (has_prefix(text, aliases[i])) {
 			text += strlen(aliases[i]);
 			*number = i;
@@ -277,12 +286,13 @@ parse_value(const char *aliases[], unsigned int *number)
 static int
 parse_range(const char *aliases[], long long *field)
 {
+	unsigned int first, last, step = 1, i;
+
 	if (*text == '*') {
 		++text;
 		return 0;
 	}
 
-	unsigned int first, last, step = 1;
 	if (parse_value(aliases, &first) < 0) return -1;
 	last = first;
 
@@ -299,7 +309,7 @@ parse_range(const char *aliases[], long long *field)
 	if (first >= 64) return -1;
 	if (last >= 64) return -1;
 	if (step < 1) return -1;
-	for (unsigned int i = first; i <= last; i += step) {
+	for (i = first; i <= last; i += step) {
 		*field |= 1ULL << i;
 	}
 	return 0;
@@ -320,6 +330,9 @@ parse_field(const char *aliases[], long long *field)
 static int
 parse_command(char **command)
 {
+	int esc = 0;
+	char *out, c, *delim;
+
 	/* Guard against empty commands. */
 	if (text >= eol) return -1;
 	
@@ -328,10 +341,9 @@ parse_command(char **command)
 	}
 
 	/* Decode escaped string into its raw form. */
-	int esc = 0;
-	char *out = *command;
+	out = *command;
 	while (text < eol) {
-		char c = *text++;
+		c = *text++;
 		if (c == '%') {
 			if (esc) --out;
 			else c = '\n';
@@ -343,7 +355,7 @@ parse_command(char **command)
 	*out++ = 0;
 	
 	/* Split command from stdin data. */
-	char *delim = strchr(*command, '\n');
+	delim = strchr(*command, '\n');
 	if (delim == NULL) delim = out;
 	*delim = 0;
 
@@ -431,17 +443,19 @@ parse_file(const char *filename)
 static void
 parse_everything(void)
 {
+	DIR *dir;
+	struct dirent *ent;
+
 	/* Yes, there's a race condition here, but all you can achieve with it
 	 * is making ocrond exit on startup - there's easier ways to do that anyway. */
 	if (!(access(CRONTAB, F_OK) < 0)) {
 		parse_file(CRONTAB);
 	}
 
-	DIR *dir;
 	if ((dir = opendir(CRON_D)) != NULL) {
 		for (;;) {
 			errno = 0;
-			struct dirent *ent = readdir(dir);
+			ent = readdir(dir);
 			if (ent == NULL) {
 				if (errno) die("Couldn't walk %s: %m", CRON_D);
 				else break;
@@ -457,21 +471,27 @@ parse_everything(void)
 static void
 run_job(int idx)
 {
+	char *inname, *indata;
+	size_t inlen;
+	ssize_t ret;
+	pid_t pid;
+	int infd, outfd;
+
 	/* Fork and return immediately in the parent process. */
-	pid_t pid = fork();
+	pid = fork();
 	if (pid < 0) syslog(LOG_EMERG, "fork: %m");
 	if (pid != 0) return;
 
 	/* Write input data to a temporary file and make stdin point to it. */
-	char *inname = strdup(STDIN_TEMP);
+	inname = strdup(STDIN_TEMP);
 	if (inname == NULL) die("strdup: %m");
-	int infd = mkstemp(inname);
+	infd = mkstemp(inname);
 	if (infd < 0) die("mkstemp: %m");
 	if (unlink(inname) < 0) die("unlink: %m");
-	char *indata = strchr(jobs[idx].command, 0) + 1;
-	size_t inlen = strlen(indata);
+	indata = strchr(jobs[idx].command, 0) + 1;
+	inlen = strlen(indata);
 	while (inlen > 0) {
-		ssize_t ret = write(infd, indata, inlen);
+		ret = write(infd, indata, inlen);
 		if (ret < 0) {
 			syslog(LOG_ERR, "write: %m");
 			exit(EXIT_FAILURE);
@@ -483,7 +503,7 @@ run_job(int idx)
 
 	/* Divert stdout to /dev/null. */
 	/* TODO Write this into a log dir instead! */
-	int outfd = open("/dev/null", O_WRONLY);
+	outfd = open("/dev/null", O_WRONLY);
 	if (outfd < 0) die("open: %m");
 	dup2(outfd, STDOUT_FILENO);
 
@@ -496,10 +516,14 @@ run_job(int idx)
 int
 main()
 {
+	char buf[100];
+	struct tm tm;
+	struct sigaction ign;
+	time_t now, target;
+
 	openlog(LOGIDENT, LOG_CONS, LOG_CRON);
 
 	/* Reap any finished child processes automatically. */
-	struct sigaction ign;
 	ign.sa_handler = SIG_IGN;
 	sigemptyset(&ign.sa_mask);
 	ign.sa_flags = SA_RESTART | SA_NOCLDSTOP | SA_NOCLDWAIT;
@@ -516,26 +540,23 @@ main()
 		die("Must have at least one job.");
 	}
 
-	time_t now;
 	time(&now);
 	init_jobs(now);
 
 	for (;;) {
-		struct tm tm;
 		localtime_r(&jobs[0].time, &tm);
-		char buf[100];
 		strftime(buf, sizeof(buf), "%H:%M, %d %b %Y", &tm);
 		syslog(LOG_DEBUG, "Next job will be run at %s.", buf);
 
 		/* FIXME Handle cases where the system clock gets set back significantly! */
-		time_t target = jobs[0].time;
+		target = jobs[0].time;
 		for (;;) {
 			time(&now);
 			if (target <= now) break;
 			sleep(target - now);
 		}
 
-		if (now - target < CATCHUP_LIMIT) {
+		if (now - target < CATCHUP_LIMIT * 60) {
 			run_job(0);
 		} else {
 			syslog(LOG_WARNING, "Job '%s' had to be skipped because it was too far in the past. (Was the system time changed?)", jobs[0].command);
